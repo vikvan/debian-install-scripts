fb3c366faf029fcc270e280da4be1a9bd6d3ad39
Viktor Vanin <vik.vanin@gmail.com>
2017-02-23 16:57:32 +0300

    remove configure_software.sh

--- post_install.sh
+++ post_install.sh
@@ -18,7 +18,40 @@ sudo apt-get dist-upgrade
 sudo ./install_software.sh
 
 #Configure software
-sudo ./configure_software.sh
+
+# Configure bash
+echo 'set -o vi' >> ~/.bashrc
+
+# Configure other soft
+cd ./etc
+
+sudo cp lightdm-xsession.desktop /usr/share/xsessions/
+sudo cp avatar-default.png /usr/share/icons/Adwaita/256x256/status/
+
+mkdir -p "$HOME/.config"
+
+cp -r dwm/ ~/.dwm
+cp fonts.conf ~/.fonts.conf
+cp -r ranger/ ~/.config/ranger
+cp -r vimperator/ ~/.vimperator
+cp vimperatorrc ~/.vimperatorrc
+cp vimrc ~/.vimrc
+cp xsessionrc ~/.xsessionrc
+cp ycm_extra_conf.py ~/.ycm_extra_conf.py
+cp -r zathura/ ~/.config/zathura
+
+cd ..
+
+git clone https://github.com/vikvan/Xdefaults-urxvt.git
+cp Xdefaults-urxvt/Xdefaults ~/.Xdefaults
+rm -rf Xdefaults-urxvt
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
+
+cd -
 
 #Configure GRUB
 sudo cp ./etc/grub /etc/default/grub
